cd095d5f230ba13782c4fa1ff586ef0abd12cc83
Krishna Kumar A <krishnakumar.a@optimuslogic.in>
2017-10-29 14:11:53 +0530

    initial commit of ntshell(+arduino port) component for use with esp-idf projects

---
+++ component.mk
@@ -0,0 +1,16 @@
+# ntshell component makefile for arduino ported ntshell for esp-idf
+
+# common includes
+COMPONENT_ADD_INCLUDEDIRS := common/include
+
+# common source files
+COMPONENT_SRCDIRS := common
+
+
+# arduino includes
+COMPONENT_ADD_INCLUDEDIRS += arduino/include
+
+# arduino source files
+COMPONENT_SRCDIRS += arduino
+
+
